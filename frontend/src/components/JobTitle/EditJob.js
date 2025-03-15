5ae69fc074c2046226105cda860d1bbd6e20c002
RySel <104247395+Ryalynne@users.noreply.github.com>
2025-03-15 20:53:52 +0800

    Update EditJob.js

--- frontend/src/components/JobTitle/EditJob.js
+++ frontend/src/components/JobTitle/EditJob.js
@@ -37,7 +37,10 @@ const EditSalary = () => {
 
   return (
     <div>
-      <div className="column is-half container mt-5">
+            <div
+        className="column p-5"
+        style={{ marginTop: "100px", position: "relative", zIndex: 1 }}
+      >
         <form onSubmit={saveJob}>
           {/* Job Title */}
           <div className="field">
